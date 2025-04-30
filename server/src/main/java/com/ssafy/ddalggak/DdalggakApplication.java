f07a197f8266fb5a44e8550dafb5177655b44898
이다영 <daosp1023@gmail.com>
2025-04-30 17:51:32 +0900

    fix: Test코드 작성 및 설정파일 수정

--- server/src/main/java/com/ssafy/ddalggak/DdalggakApplication.java
+++ server/src/main/java/com/ssafy/ddalggak/DdalggakApplication.java
@@ -1,13 +1,13 @@
-package com.ssafy.server;
+package com.ssafy.ddalggak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class DdalggakApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication.run(DdalggakApplication.class, args);
 	}
 
 }
---
+++ server/src/main/java/com/ssafy/ddalggak/Test/TestController.java
@@ -0,0 +1,15 @@
+package com.ssafy.ddalggak.Test;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
