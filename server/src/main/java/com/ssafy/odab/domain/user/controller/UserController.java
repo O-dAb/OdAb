9cd37ad05cab663d39c516004eb3a2e3b3fd6ebb
이다영 <daosp1023@gmail.com>
2025-05-09 17:54:38 +0900

    feat: profile-img 테스트용 컨트롤러 작성

--- server/src/main/java/com/ssafy/odab/domain/user/controller/UserController.java
+++ server/src/main/java/com/ssafy/odab/domain/user/controller/UserController.java
@@ -2,19 +2,20 @@ package com.ssafy.odab.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class UserController {
 
-  @PostMapping("/login")
-  public ResponseEntity<String> doLogin() {
-
-    return ResponseEntity.ok("서버 되는거지 데브툴스도 된다.");
+  @PutMapping("profile_img")
+  public ResponseEntity<String> updateProfileImg(@RequestParam("profile_img") String profileImg) {
+    return ResponseEntity.ok().body(profileImg);
   }
+
+
+
+
 
 }
