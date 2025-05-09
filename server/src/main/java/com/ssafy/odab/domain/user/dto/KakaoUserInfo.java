9fdf0fa1f0b8058a2f73d8cebb57aadc04d2f911
함동건 <hdk5190@gmail.com>
2025-05-09 10:04:38 +0900

    a

---
+++ server/src/main/java/com/ssafy/odab/domain/user/dto/KakaoUserInfo.java
@@ -0,0 +1,13 @@
+package com.ssafy.odab.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KakaoUserInfo {
+    private Integer id;
+    private String nickname;
+}
