5cded00cedf8ef31b972aadc7e714b1f85639821
함동건 <hdk5190@gmail.com>
2025-05-14 09:32:39 +0900

    feat:login

--- server/src/main/java/com/ssafy/odab/domain/user/service/JwtService.java
+++ server/src/main/java/com/ssafy/odab/domain/user/service/JwtService.java
@@ -12,7 +12,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import jakarta.annotation.PostConstruct;
 import java.security.Key;
 import java.util.Date;
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 
 @Service
