a2ffb6366e58cb0d3c3f9d0b843023df5b0aa21e
dongs <don0421865@gmail.com>
2025-05-19 13:14:21 +0900

    fix: token -> accessToken으로 수정

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -13,7 +13,7 @@ const authInstance = axios.create({
 
 authInstance.interceptors.request.use(
   (config) => {
-    const accessToken = localStorage.getItem("token");
+    const accessToken = localStorage.getItem("accessToken");
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
