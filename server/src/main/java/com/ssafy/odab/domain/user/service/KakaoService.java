b0aae963a0c84945eccffe42605fa41be7ccc173
함동건 <hdk5190@gmail.com>
2025-05-20 15:35:15 +0900

    a

--- server/src/main/java/com/ssafy/odab/domain/user/service/KakaoService.java
+++ server/src/main/java/com/ssafy/odab/domain/user/service/KakaoService.java
@@ -140,20 +140,13 @@ public class KakaoService {
         String uuid = UUID.randomUUID().toString();
 
         // 4. Redis에 JSON으로 저장 (3분 유효)
-<<<<<<< HEAD
-        String profileUrl = user.getProfileUrl();
-        if (profileUrl == null || profileUrl.trim().isEmpty()) {
-            profileUrl = "/default-profile.png"; // 기본 이미지 경로
-        }
-=======
->>>>>>> fb5bc89e50040ac31e1c42579e71fc9513e87aed
+
         Map<String, Object> redisData = new HashMap<>();
         redisData.put("accessToken", accessToken);
         redisData.put("refreshToken", refreshToken);
         redisData.put("userId", user.getId());
         redisData.put("nickname", user.getUserName());
         redisData.put("grade", user.getGrade());
-        redisData.put("profileUrl", user.getProfileUrl() != null ? user.getProfileUrl() : "");
         redisData.put("profileUrl", user.getProfileUrl() != null ? user.getProfileUrl() : ""); // url null 처리
         try {
             String redisValue = objectMapper.writeValueAsString(redisData);
