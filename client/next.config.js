b742b7c4f57982d536bff845829253b7d4ad2a6e
Lee DaYoung <daosp1023@gmail.com>
2025-05-20 19:31:48 +0900

    fix: client dockerfile 수정

--- client/next.config.js
+++ client/next.config.js
@@ -1,29 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   output: 'standalone',
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // 타입스크립트 오류 무시
   },
   eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // 메모리 사용량 줄이기 위한 설정
-  experimental: {
-    largePageDataBytes: 128 * 1000, // 기본값 보다 작게 설정
-  },
-  // 청크 최적화 설정
-  webpack: (config) => {
-    // 불필요한 것들 제외
-    if (!config.isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'fs': false,
-        'path': false,
-      };
-    }
-    return config;
-  },
+    ignoreDuringBuilds: true, // ESLint 오류 무시
+  }
 };
 
 module.exports = nextConfig;
